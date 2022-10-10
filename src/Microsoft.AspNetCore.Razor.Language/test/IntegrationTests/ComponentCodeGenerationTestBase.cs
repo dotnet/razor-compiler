f16f7aea7691442c9d8fe811037f43f738d3bd28
jacalvar <jacalvar@microsoft.com>
2022-10-10 17:50:15 +0200

    Add previous tests

--- src/Microsoft.AspNetCore.Razor.Language/test/IntegrationTests/ComponentCodeGenerationTestBase.cs
+++ src/Microsoft.AspNetCore.Razor.Language/test/IntegrationTests/ComponentCodeGenerationTestBase.cs
@@ -1933,6 +1933,195 @@ namespace Test
     }
 
     [Fact]
+    public void BindToComponent_WithGetSet_EventCallback()
+    {
+        // Arrange
+        AdditionalSyntaxTrees.Add(Parse(@"
+using System;
+using Microsoft.AspNetCore.Components;
+namespace Test
+{
+    public class MyComponent : ComponentBase
+    {
+        [Parameter]
+        public int Value { get; set; }
+        [Parameter]
+        public EventCallback<int> ValueChanged { get; set; }
+    }
+}"));
+
+        // Act
+        var generated = CompileToCSharp(@"
+<MyComponent @bind-Value:get=""ParentValue"" @bind-Value:set=""UpdateValue"" />
+@code {
+    public int ParentValue { get; set; } = 42;
+    public EventCallback<int> UpdateValue { get; set; }
+}");
+
+        // Assert
+        AssertDocumentNodeMatchesBaseline(generated.CodeDocument);
+        AssertCSharpDocumentMatchesBaseline(generated.CodeDocument);
+        var result = CompileToAssembly(generated, throwOnFailure: false);
+
+        var error = Assert.Single(result.Diagnostics);
+        // Argument 1: cannot convert from 'Microsoft.AspNetCore.Components.EventCallback' to 'System.Action'
+        Assert.Equal("CS1503", error.Id);
+    }
+
+    [Fact]
+    public void BindToGenericComponent_InferredType_WithGetSet_EventCallback()
+    {
+        // Arrange
+        AdditionalSyntaxTrees.Add(Parse(@"
+using System;
+using Microsoft.AspNetCore.Components;
+namespace Test
+{
+    public class MyComponent<TValue> : ComponentBase
+    {
+        [Parameter]
+        public TValue Value { get; set; }
+        [Parameter]
+        public EventCallback<TValue> ValueChanged { get; set; }
+    }
+    public class CustomValue
+    {
+    }
+}"));
+
+        // Act
+        var generated = CompileToCSharp(@"
+<MyComponent @bind-Value:get=""ParentValue"" @bind-Value:set=""UpdateValue"" />
+@code {
+    public CustomValue ParentValue { get; set; } = new CustomValue();
+    public EventCallback<CustomValue> UpdateValue { get; set; }
+}");
+
+        // Assert
+        AssertDocumentNodeMatchesBaseline(generated.CodeDocument);
+        AssertCSharpDocumentMatchesBaseline(generated.CodeDocument);
+        var result = CompileToAssembly(generated, throwOnFailure: false);
+
+        var error = Assert.Single(result.Diagnostics);
+        // Argument 1: cannot convert from 'Microsoft.AspNetCore.Components.EventCallback' to 'System.Action'
+        Assert.Equal("CS1503", error.Id);
+    }
+
+    [Fact]
+    public void BindToGenericComponent_ExplicitType_WithGetSet_EventCallback()
+    {
+        // Arrange
+        AdditionalSyntaxTrees.Add(Parse(@"
+using System;
+using Microsoft.AspNetCore.Components;
+namespace Test
+{
+    public class MyComponent<TValue> : ComponentBase
+    {
+        [Parameter]
+        public TValue Value { get; set; }
+        [Parameter]
+        public EventCallback<TValue> ValueChanged { get; set; }
+    }
+    public class CustomValue
+    {
+    }
+}"));
+
+        // Act
+        var generated = CompileToCSharp(@"
+<MyComponent TValue=""CustomValue"" @bind-Value:get=""ParentValue"" @bind-Value:set=""UpdateValue"" />
+@code {
+    public CustomValue ParentValue { get; set; } = new CustomValue();
+    public EventCallback<CustomValue> UpdateValue { get; set; }
+}");
+
+        // Assert
+        AssertDocumentNodeMatchesBaseline(generated.CodeDocument);
+        AssertCSharpDocumentMatchesBaseline(generated.CodeDocument);
+        var result = CompileToAssembly(generated, throwOnFailure: false);
+
+        var error = Assert.Single(result.Diagnostics);
+        // Argument 1: cannot convert from 'Microsoft.AspNetCore.Components.EventCallback' to 'System.Action'
+        Assert.Equal("CS1503", error.Id);
+
+    }
+
+    [Fact]
+    public void GenericComponentBindToGenericComponent_InferredType_WithGetSet_EventCallback()
+    {
+        // Arrange
+        AdditionalSyntaxTrees.Add(Parse(@"
+using System;
+using Microsoft.AspNetCore.Components;
+namespace Test
+{
+    public class MyComponent<TValue> : ComponentBase
+    {
+        [Parameter]
+        public TValue Value { get; set; }
+        [Parameter]
+        public EventCallback<TValue> ValueChanged { get; set; }
+    }
+}"));
+
+        // Act
+        var generated = CompileToCSharp(@"
+@typeparam TParam
+<MyComponent @bind-Value:get=""ParentValue"" @bind-Value:set=""UpdateValue"" />
+@code {
+    public TParam ParentValue { get; set; } = default;
+    public EventCallback<TParam> UpdateValue { get; set; }
+}");
+
+        // Assert
+        AssertDocumentNodeMatchesBaseline(generated.CodeDocument);
+        AssertCSharpDocumentMatchesBaseline(generated.CodeDocument);
+        var result = CompileToAssembly(generated, throwOnFailure: false);
+
+        var error = Assert.Single(result.Diagnostics);
+        // Argument 1: cannot convert from 'Microsoft.AspNetCore.Components.EventCallback' to 'System.Action'
+        Assert.Equal("CS1503", error.Id);
+    }
+
+    [Fact]
+    public void GenericBindToGenericComponent_ExplicitType_WithGetSet_EventCallback()
+    {
+        // Arrange
+        AdditionalSyntaxTrees.Add(Parse(@"
+using System;
+using Microsoft.AspNetCore.Components;
+namespace Test
+{
+    public class MyComponent<TValue> : ComponentBase
+    {
+        [Parameter]
+        public TValue Value { get; set; }
+        [Parameter]
+        public EventCallback<TValue> ValueChanged { get; set; }
+    }
+}"));
+
+        // Act
+        var generated = CompileToCSharp(@"
+@typeparam TParam
+<MyComponent TValue=""TParam"" @bind-Value:get=""ParentValue"" @bind-Value:set=""UpdateValue"" />
+@code {
+    public TParam ParentValue { get; set; } = default;
+    public EventCallback<TParam> UpdateValue { get; set; }
+}");
+
+        // Assert
+        AssertDocumentNodeMatchesBaseline(generated.CodeDocument);
+        AssertCSharpDocumentMatchesBaseline(generated.CodeDocument);
+        var result = CompileToAssembly(generated, throwOnFailure: false);
+
+        var error = Assert.Single(result.Diagnostics);
+        // Argument 1: cannot convert from 'Microsoft.AspNetCore.Components.EventCallback' to 'System.Action'
+        Assert.Equal("CS1503", error.Id);
+    }
+
+    [Fact]
     public void BindToGenericComponent_InferredType_WithGetSet_Action()
     {
         // Arrange
@@ -2299,6 +2488,42 @@ namespace Test
         AssertDocumentNodeMatchesBaseline(generated.CodeDocument);
         AssertCSharpDocumentMatchesBaseline(generated.CodeDocument);
         CompileToAssembly(generated);
+    }
+
+    [Fact]
+    public void BindToComponent_WithAfter_EventCallback()
+    {
+        // Arrange
+        AdditionalSyntaxTrees.Add(Parse(@"
+using System;
+using Microsoft.AspNetCore.Components;
+namespace Test
+{
+    public class MyComponent : ComponentBase
+    {
+        [Parameter]
+        public int Value { get; set; }
+        [Parameter]
+        public EventCallback<int> ValueChanged { get; set; }
+    }
+}"));
+
+        // Act
+        var generated = CompileToCSharp(@"
+<MyComponent @bind-Value:get=""ParentValue"" @bind-Value:after=""UpdateValue"" />
+@code {
+    public int ParentValue { get; set; } = 42;
+    public EventCallback UpdateValue { get; set; }
+}");
+
+        // Assert
+        AssertDocumentNodeMatchesBaseline(generated.CodeDocument);
+        AssertCSharpDocumentMatchesBaseline(generated.CodeDocument);
+        var result = CompileToAssembly(generated, throwOnFailure: false);
+
+        var error = Assert.Single(result.Diagnostics);
+        // Argument 1: cannot convert from 'Microsoft.AspNetCore.Components.EventCallback' to 'System.Action'
+        Assert.Equal("CS1503", error.Id);
     }
 
     [Fact]
